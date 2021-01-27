6b08e2eed352b52812ea109db13eb44eba320fb0
LiShanglin <LiShanglin@DESKTOP-0ACGH0P>
2021-01-27 22:06:57 +0800

    添加项目文件。

---
+++ p_printf_exercise.cpp
@@ -0,0 +1,13 @@
+﻿#include<stdio.h>
+int main(void)
+{
+	float input;
+	printf("Enter a floating-point value:");
+	scanf_s("%f", &input);
+	printf("fixed-point notation: %f \n", input);
+	printf("exponential notation: %e \n", input);
+	printf("p notation: %a \n", input);
+
+	return 0;
+
+}
